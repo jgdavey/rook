ccf268f256106271950dd1393655789feab4c5db
Joshua Davey <josh@joshuadavey.com>
2014-05-31 14:12:55 -0500

    Add easier build script
    Just run assets/build from project root dir

--- assets/config.rb
+++ assets/config.rb
@@ -55,18 +55,5 @@ set :build_dir, '../resources/public'
 
 # Build-specific configuration
 configure :build do
-  # For example, change the Compass output style for deployment
-  # activate :minify_css
-
-  # Minify Javascript on build
-  # activate :minify_javascript
-
-  # Enable cache buster
-  # activate :asset_hash
-
-  # Use relative URLs
-  # activate :relative_assets
-
-  # Or use a different image path
-  # set :http_prefix, "/Content/images/"
+  activate :minify_css
 end
