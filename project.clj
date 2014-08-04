a758c3e9765b9e9115aa36d670f8129f97945fee
Joshua Davey <josh@joshuadavey.com>
2014-08-04 15:06:08 -0500

    Bump dependencies

--- project.clj
+++ project.clj
@@ -4,16 +4,16 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript  "0.0-2227"]
+                 [org.clojure/clojurescript "0.0-2280"]
                  [org.clojure/core.async "0.1.303.0-886421-alpha"]
                  [com.stuartsierra/component "0.2.1"]
                  [http-kit "2.1.18"]
-                 [ring "1.2.2"]
-                 [com.taoensso/sente "0.14.1"]
+                 [ring "1.3.0"]
+                 [com.taoensso/sente "0.15.1"]
                  [myguidingstar/clansi "1.3.0"]
                  [cheshire "5.3.1"]
-                 [om "0.6.4"]
-                 [com.ninjudd/eventual "0.3.1"]
+                 [om "0.7.0"]
+                 [com.ninjudd/eventual "0.3.2"]
                  [com.ninjudd/ring-async "0.2.3"]
                  [compojure "1.1.8"]]
 
@@ -41,6 +41,6 @@
   :main ^:skip-aot rook.engine
 
   :profiles {:dev {:plugins [[com.cemerick/austin "0.1.4"]]
-                   :dependencies [[org.clojure/tools.namespace "0.2.4"]]
+                   :dependencies [[org.clojure/tools.namespace "0.2.5"]]
                    :source-paths ["dev"]
                    :repl-options {:init-ns user}}})
